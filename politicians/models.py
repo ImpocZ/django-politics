ef895726cd20a05714398a4e5bcfe3f700648e60
ImpocZ <knightradim.senk@gmail.com>
2026-05-14 09:06:27 +0200

    "validations"

--- politicians/models.py
+++ politicians/models.py
@@ -1,63 +1,85 @@
+from datetime import date
+
+from django.core.exceptions import ValidationError
+from django.core.validators import MaxValueValidator, MinValueValidator, RegexValidator
 from django.db import models
 
 
+alphabetic_validator = RegexValidator(
+    regex=r'^[A-ZÁČĎÉĚÍŇÓŘŠŤÚŮÝŽ][a-záčďéěíňóřšťúůýž]*$',
+    message='This field must start with a capital letter and contain only letters.',
+)
+
+
+def validate_letters_only(value):
+    normalized_value = value.replace(' ', '')
+    if not normalized_value.isalpha():
+        raise ValidationError('This field can contain only letters.')
+
+
+def validate_founding_year(value):
+    current_year = date.today().year
+    if value < 1880 or value > current_year:
+        raise ValidationError(f'The founding year must be between 1880 and {current_year}.')
+
+
 class Politician(models.Model):
-    id = models.IntegerField(primary_key=True)
-    name = models.CharField(max_length=50)
-    surname = models.CharField(max_length=50)
+    name = models.CharField(max_length=50, validators=[alphabetic_validator])
+    surname = models.CharField(max_length=50, validators=[alphabetic_validator])
     title = models.CharField(max_length=50, blank=True, null=True)
     photo = models.BinaryField(blank=True, null=True)
     photo_file = models.ImageField(upload_to='photos/politicians/', blank=True, null=True)
-    age = models.SmallIntegerField(blank=True, null=True)
+    age = models.SmallIntegerField(blank=True, null=True, validators=[MinValueValidator(21), MaxValueValidator(100)])
 
     class Meta:
         db_table = 'politician'
-
+        verbose_name = "politik"
+        verbose_name_plural = "politici"
     def __str__(self):
         return f"{self.name} {self.surname}"
 
 
 class Party(models.Model):
-    id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=50)
-    founding = models.IntegerField()
+    founding = models.IntegerField(validators=[validate_founding_year])
     leader = models.CharField(max_length=75)
     photo = models.BinaryField(blank=True, null=True)
     photo_file = models.ImageField(upload_to='photos/parties/', blank=True, null=True)
 
     class Meta:
         db_table = 'party'
+        verbose_name = 'strana'
+        verbose_name_plural = 'strany'
 
     def __str__(self):
         return self.name
 
 
 class Office(models.Model):
-    id = models.IntegerField(primary_key=True)
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=200, validators=[validate_letters_only])
     description = models.TextField(blank=True, null=True)
 
     class Meta:
         db_table = 'office'
-
+        verbose_name = 'funkce'
+        verbose_name_plural = 'funkce'
     def __str__(self):
         return self.name
 
 
 class Government(models.Model):
-    id = models.IntegerField(primary_key=True)
     in_function = models.DateField()
-    prime_minister = models.CharField(max_length=50)
+    prime_minister = models.CharField(max_length=50, validators=[validate_letters_only])
 
     class Meta:
         db_table = 'government'
-
+        verbose_name = 'vláda'
+        verbose_name_plural = 'vlády'
     def __str__(self):
         return f"Government {self.id} - {self.in_function}"
 
 
 class GovernmentOfficial(models.Model):
-    id = models.AutoField(primary_key=True)
     government = models.ForeignKey(Government, on_delete=models.CASCADE, db_column='id_government')
     politician = models.ForeignKey(Politician, on_delete=models.CASCADE, db_column='id_politician')
     party = models.ForeignKey(Party, on_delete=models.SET_NULL, null=True, blank=True, db_column='id_party')
@@ -75,21 +97,23 @@ class GovernmentOfficial(models.Model):
 
     class Meta:
         db_table = 'government_official'
+        verbose_name = "vládní funkce"
+        verbose_name_plural = "vládní funkce"
         unique_together = (('politician', 'government', 'office'),)
 
 
 class GovernmentParty(models.Model):
-    id = models.AutoField(primary_key=True)
     party = models.ForeignKey(Party, on_delete=models.CASCADE, db_column='id_party')
     government = models.ForeignKey(Government, on_delete=models.CASCADE, db_column='id_government')
 
     class Meta:
         db_table = 'government_party'
+        verbose_name = "vládnoucí strana"
+        verbose_name_plural = "vládnoucí strany"
         unique_together = (('party', 'government'),)
 
 
 class PoliticsParty(models.Model):
-    id = models.AutoField(primary_key=True)
     politician = models.ForeignKey(Politician, on_delete=models.CASCADE, db_column='id_politician')
     party = models.ForeignKey(Party, on_delete=models.CASCADE, db_column='id_party')
     start_of_membership = models.DateField()
@@ -97,4 +121,6 @@ class PoliticsParty(models.Model):
 
     class Meta:
         db_table = 'politics_party'
+        verbose_name = "strana politika"
+        verbose_name_plural = "strany politiků"
         unique_together = (('politician', 'party'),)
